99e451bd253674a9b3eb3a05112be93956ed526e
Malwina Pietruszewska <malwina.py@gmail.com>
2021-05-14 15:02:23 +0200

    6.2 Added calculator

--- Kodilla-testing/src/main/java/com/kodilla/testing/TestingMain.java
+++ Kodilla-testing/src/main/java/com/kodilla/testing/TestingMain.java
@@ -1,7 +1,26 @@
 package com.kodilla.testing;
 
+import com.kodilla.testing.calculator.Calculator;
+import com.kodilla.testing.user.SimpleUser;
+
 public class TestingMain {
-    public static void main(String [] args) {
-        System.out.println("Moduł 6. Wprowadzenie do oprogramowania");
+
+    public static void main(String[] args) {
+        Calculator calc = new Calculator(10,7);
+
+        int sumResult = calc.sumAAndB();
+
+        if (sumResult==17) {
+            System.out.println("test OK");
+        } else {
+            System.out.println("Error!");
+        }
+
+        double subResult = calc.subtractAFromB();
+        if (subResult==-3) {
+            System.out.println("test OK");
+        } else {
+            System.out.println("Error!");
+        }
     }
 }
---
+++ Kodilla-testing/src/main/java/com/kodilla/testing/calculator/Calculator.java
@@ -0,0 +1,21 @@
+package com.kodilla.testing.calculator;
+
+public class Calculator {
+    private int a;
+    private int b;
+
+    public Calculator( int a, int b){
+        this.a = a;
+        this.b = b;
+    }
+
+    public int sumAAndB() {
+        int sum = a + b;
+        return sum;
+    }
+
+    public double subtractAFromB() {
+        double subtract = b - a;
+        return subtract;
+    }
+}
