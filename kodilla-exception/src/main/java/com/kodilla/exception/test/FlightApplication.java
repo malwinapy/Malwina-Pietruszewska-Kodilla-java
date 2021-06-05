b736624199c7c09dd8caf87d9706372d7a6232ba
Malwina Pietruszewska <malwina.py@gmail.com>
2021-06-05 13:29:19 +0200

    exception FlightsFinder fixed

--- kodilla-exception/src/main/java/com/kodilla/exception/test/FlightApplication.java
+++ kodilla-exception/src/main/java/com/kodilla/exception/test/FlightApplication.java
@@ -2,20 +2,21 @@ package com.kodilla.exception.test;
 
 public class FlightApplication {
     public static void main(String[] args) {
-        FlightsCollection flightsCollection = new FlightsCollection();
+        FlightsFinder flightsFinder = new FlightsFinder();
         Flight flight1 = new Flight("Brussels", "Warsaw");
         Flight flight2 = new Flight("Eindhoven", "Katowice");
         Flight flight3 = new Flight("Paris", "Rome");
 
         try {
-            boolean possibleFlight = flightsCollection.findFlight(flight3);
-            if (possibleFlight){
-                System.out.println("Flight possible");
-            } else {
-                System.out.println("Flight impossible");
-            }
+            //flightsFinder.findFlight(flight1);
+            //flightsFinder.findFlight(flight2);
+            flightsFinder.findFlight(flight3);
+
         } catch (RouteNotFoundException e){
-            System.out.println("arrivalAirport does not exist");
+            System.out.println("Flight not found");
+        }
+        finally {
+            System.out.println("***");
         }
     }
 }
---
+++ kodilla-exception/src/main/java/com/kodilla/exception/test/FlightsFinder.java
@@ -0,0 +1,39 @@
+package com.kodilla.exception.test;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class FlightsFinder {
+    void findFlight(Flight flight) throws RouteNotFoundException {
+        Map<String, Boolean> flightCollection = new HashMap<>();
+
+        flightCollection.put("Warsaw", true);
+        flightCollection.put("Brussels", false);
+        flightCollection.put("New York", true);
+        flightCollection.put("Madrid", true);
+        flightCollection.put("Paris", false);
+        flightCollection.put("London", true);
+        flightCollection.put("Rome", false);
+        flightCollection.put("Amsterdam", false);
+
+        // return Optional <String> possibleFly = flightCollection.entrySet().stream()
+        // .filter(f -> flight.getArrivalAirport.equals(f.getKey))
+        // .findFirst();
+        // .orElseThrow(() -> new RouteNotFoundException("Route not found")
+        // assertEquals(flight.getArrivalAirport, f.getKey)
+        for (Map.Entry<String, Boolean> map : flightCollection.entrySet()) {
+            if (map.getKey().equals(flight.getDepartureAirport())) {
+                if (map.getKey().equals(flight.getArrivalAirport()) && (map.getValue().equals(true))) {
+                    System.out.println("Flight from: " + flight.getDepartureAirport() + " to " + flight.getArrivalAirport());
+                    System.out.println("Flight available");
+                    System.out.println(" ");
+
+                }
+            }else {
+                    System.out.println("Flight from: " + flight.getDepartureAirport() + " to " + flight.getArrivalAirport());
+                    throw new RouteNotFoundException();
+                }
+            }
+        }
+    }
+
