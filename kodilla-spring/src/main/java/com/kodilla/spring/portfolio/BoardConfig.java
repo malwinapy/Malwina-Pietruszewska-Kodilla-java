65cb8697a1743d0cc765167de3262e2271256772
Malwina Pietruszewska <malwina.py@gmail.com>
2021-09-13 17:15:56 +0200

    14.3 Board

--- kodilla-spring/src/main/java/com/kodilla/spring/portfolio/BoardConfig.java
+++ kodilla-spring/src/main/java/com/kodilla/spring/portfolio/BoardConfig.java
@@ -1,4 +1,33 @@
 package com.kodilla.spring.portfolio;
 
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Scope;
+
+@Configuration
 public class BoardConfig {
+
+    @Bean(name = "toDoLst")
+    @Scope("prototype")
+    public TaskList getToDoList(){
+        return new TaskList();
+    }
+
+    @Bean(name = "inProgressList")
+    @Scope("prototype")
+    public TaskList getInProgressList(){
+        return new TaskList();
+    }
+
+    @Bean(name = "doneList")
+    @Scope("prototype")
+    public TaskList getDoneList(){
+        return new TaskList();
+    }
+
+
+    @Bean
+    public Board getBoard(){
+        return new Board(getToDoList(), getInProgressList(), getDoneList());
+    }
 }
--- kodilla-spring/src/test/java/com/kodilla/spring/portfolio/BoardTestSuite.java
+++ kodilla-spring/src/test/java/com/kodilla/spring/portfolio/BoardTestSuite.java
@@ -1,4 +1,36 @@
 package com.kodilla.spring.portfolio;
 
+import com.kodilla.kodillaspring.KodillaSpringApplication;
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+import java.util.List;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+
+@SpringBootTest(classes={KodillaSpringApplication.class})
 public class BoardTestSuite {
+
+    @Test
+    void testTaskAdd() {
+        //Given
+        ApplicationContext context = new AnnotationConfigApplicationContext(BoardConfig.class);
+        Board board = context.getBean(Board.class);
+
+        //When
+        List<String> doneList = board.getDoneList().getTasks();
+        List<String> inProgressList = board.getInProgressList().getTasks();
+        List<String> toDoList = board.getToDoList().getTasks();
+
+        doneList.add("live");
+        inProgressList.add("laugh");
+        toDoList.add("love");
+
+        //Then
+        assertEquals("live", doneList.get(0));
+        assertEquals("laugh", inProgressList.get(0));
+        assertEquals("love", toDoList.get(0));
+
+    }
 }
